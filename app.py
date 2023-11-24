b932032e47653f5eb6328f688ca76ae2ba36dbe9
Your Name <2100009162@kluniversity.in>
2023-11-24 20:45:33 +0530

    MongoDB Connection Establishment

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/SAS"
+db= PyMongo(app).db
 
 
 @app.route('/')
 def function():
+  db.users.insert_one({"b": 2})
   return render_template("index.html")
 
 
